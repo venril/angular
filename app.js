dffb4665a968b56b6794d2a4edc432b0efd86315
venril <toto@example.com>
2016-01-11 14:27:39 +0100

    Etape 1.4.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
-  var gem = { name: 'Azurite', price: 2.95 };
+  var gem = { name: 'Azurite', price: 2.95, quality: 'low' };
   var app = angular.module('gemStore', []);
   app.controller('StoreController',function(){
     this.product = gem;
